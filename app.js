f3466161bc99e22ef5d6f785e6bfa1191e782486
Rana Arju <arjurana530@gmail.com>
2022-03-01 19:37:16 +0600

    search result show functionality add

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ const searchBtn = () => {
         .then(response => response.json())
         .then(data => showPhone(data.data));
 }
+const showPhone = data => {
+    const container = document.getElementById("container");
+    for (const info of data) {
+        const div = document.createElement("div");
+        div.className = "col-md-4 ";
+        div.innerHTML = `
+        <div class="card mb-3 mx-auto" style="width: 18rem;">
+            <img src="${info.image}" class="card-img-top" width ="200px" height="100%">
+                <div class="card-body">
+                    <h5 class="card-title">Name: ${info.brand}</h5>
+                    <p class="card-text">Model: ${info.phone_name}</p>
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-primary custom-btn rounded-pill" id ="details">Details</button>
+                </div>
+                </div>
+        `;
+        container.appendChild(div);
+    }
+}
